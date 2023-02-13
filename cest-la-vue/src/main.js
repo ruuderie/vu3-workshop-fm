3c6018de0e136ebac48191a5b74543ba65442e9d
salym.eth <basset-zircon-0l@icloud.com>
2023-02-13 18:50:30 -0300

    added changes to routing

--- cest-la-vue/src/main.js
+++ cest-la-vue/src/main.js
@@ -1,4 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomePage from "@/views/HomePage.vue";
+import UserPage from "@/views/UserPage.vue";
+import LoginPage from "@/views/LoginPage.vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App)
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+    {
+        path: "/",
+        component: HomePage,
+    },
+    {
+        path: "/login",
+        component:  () => import("@/views/LoginPage.vue"),
+    },
+    {
+        path: "/user",
+        component: () => import("@/views/UserPage.vue"),
+    }
+    ]
+})
+
+app.use(router)
+app.mount("#app")
